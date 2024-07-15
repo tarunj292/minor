5f38a52365457f9d7d880db343dd86f50c5e029c
DishankV <160301804+DishankV@users.noreply.github.com>
2024-07-16 00:11:01 +0530

    Updated controllers and routes

--- backend/routes/minorRoutes.js
+++ backend/routes/minorRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { createStudent, getAllMinors } = require("../controllers/minorControllers");
+const { createStudent, getAllMinors, getAllStudentByMinor,getAllPrograms } = require("../controllers/minorControllers");
 
 router.get("/getMinor", getAllMinors);
-// router.get("/getProgram", getAllPrograms);
+router.get("/getProgram", getAllPrograms);
 router.patch("/createStudent", createStudent);
+router.get("/getStudent/:_minorName",getAllStudentByMinor);
 
 module.exports = router;
